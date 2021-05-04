f84b3101ba3a4b89ec8ebb0bcd26b67da5f594ea
김남준 <namjun88@gmail.com>
2021-05-04 23:18:40 +0900

    count 갯수 일급 콜레션

---
+++ src/main/java/race/TryCount.java
@@ -0,0 +1,20 @@
+package race;
+
+public class TryCount {
+
+	private Integer tryCount;
+
+	public TryCount(Integer tryCount) {
+		this.tryCount = tryCount;
+	}
+
+	public Integer getTryCount() {
+		return tryCount;
+	}
+
+	public void setTryCount(Integer tryCount) {
+		this.tryCount = tryCount;
+	}
+	
+	
+}
