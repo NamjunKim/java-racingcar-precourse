e0ae5d207ae2a489b6b9c789337e910d4fbb86ae
김남준 <namjun88@gmail.com>
2021-05-04 23:13:28 +0900

    Car name 클래스 함수 구

---
+++ src/main/java/race/CarName.java
@@ -0,0 +1,19 @@
+package race;
+
+public class CarName {
+	
+	private String name;
+	 	
+	public CarName () {
+		this.name = "";
+	}
+	
+	public void setCarName(String name) {
+		this.name = name;
+	}
+	
+	public String getCarName() {
+		return this.name;
+	}
+
+}
